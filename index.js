04515dcb05146f5ae0a1856a2ffe10f0cd7dd530
James Halliday <mail@substack.net>
2016-07-21 23:37:31 -0700

    incorporate K

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 var PI = Math.PI
-var thetax = Math.asin(2/3) // transition latitude
-var H = 4
+var H = 4, K = 3
+var thetax = Math.asin((K-1)/K) // transition latitude
 
 exports.ang2xy = ang2xy
 function ang2xy (out, phi, theta) { // longitude, latitude radians
+  var omega = K % 2 === 1 || theta > 0 ? 1 : 0
   if (Math.abs(theta) > thetax) { // polar
     var sign = theta > 0 ? -1 : 1
-    var sigma = Math.sqrt(3*(1-Math.abs(Math.sin(theta))))
-    var phic = -PI+(2*Math.floor((phi+PI)*H/(2*PI))+1)*PI/H
+    var sigma = Math.sqrt(K*(1-Math.abs(Math.sin(theta))))
+    var phic = -PI+(2*Math.floor((phi+PI)*H/(2*PI)+(1-omega)/2)+omega)*PI/H
     var x = phic + (phi - phic) * sigma
-    var y = sign * PI / H * (2 - sigma)
+    var y = sign * PI / H * ((K+1)/2 - sigma)
   } else { // equitorial
     var x = phi
-    var y = 3*PI/(2*H)*Math.sin(theta)
+    var y = K*PI/(2*H)*Math.sin(theta)
   }
   out[0] = x
   out[1] = y
