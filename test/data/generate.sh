9941ec510862b081e1422e1443aa03cf60cc1b64
James Halliday <mail@substack.net>
2016-07-22 01:12:25 -0700

    whatever

--- test/data/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PI=3.141592653589793
-LON=$1
-LAT=$2
-THETA=`echo "scale=8;$LAT/180*$PI"|bc`
-PHI=`echo "scale=8;$LON/180*$PI"|bc`
-
-echo $THETA $PHI | proj +proj=healpix -f '%.8f' +lon_0=0 +a=1 -E
