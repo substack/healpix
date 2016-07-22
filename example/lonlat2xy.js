7f34139b9c11b1603edf667fdea1b45826259be4
James Halliday <mail@substack.net>
2016-07-22 00:28:12 -0700

    fix bug in example, inverts correctly

--- example/lonlat2xy.js
+++ example/lonlat2xy.js
@@ -1,5 +1,5 @@
 var healpix = require('../')
-var theta = Number(process.argv[2]) / 180 * Math.PI
-var phi = Number(process.argv[3]) / 180 * Math.PI
+var phi = Number(process.argv[2]) / 180 * Math.PI
+var theta = Number(process.argv[3]) / 180 * Math.PI
 var out = healpix.ang2xy([], phi, theta)
 console.log(out.join(' '))
